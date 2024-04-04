8636ccf8c3ed71717d4887b55d75c73c4e135d73
Lenny Thobejane <Lennythobejane@gmail.com>
2024-04-04 12:17:20 +0200

    handled the form submission

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,46 @@
+import { useState } from "react";
+
 const LoginForm = () => {
+	// states
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	// functions
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const username = formData.get("username");
+		const password = formData.get("password");
+
+		// here we have to make the post to the backend and see if the user exists
+		console.log("Username:", username);
+		console.log("Password:", password);
+	};
 	return (
 		<div>
 			<h3>Login</h3>
-			<div>
-				<label htmlFor="username">Username</label>
-				<input type="text" id="username" />
-			</div>
-			<div>
-				<label htmlFor="password">Password</label>
-				<input type="password" id="password" />
-			</div>
-			<br />
-			<button type="submit">Login</button>
+			<form onSubmit={handleFormSubmit}>
+				<div>
+					<label htmlFor="username">Username</label>
+					<input
+						name="username"
+						type="text"
+						id="username"
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input
+						name={"password"}
+						type="password"
+						id="password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<br />
+				<button type="submit">Login</button>
+			</form>
 		</div>
 	);
 };
