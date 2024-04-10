5d697684bf9c69df19acc391080e03fe33b9e844
Lenny Thobejane <Lennythobejane@gmail.com>
2024-04-10 09:21:43 +0200

    exercise 5.5 done

--- src/App.jsx
+++ src/App.jsx
@@ -7,55 +7,70 @@ import LoginForm from "./components/LoginForm";
 import NotificationMessage from "./components/NotificationMessage";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [user, setUser] = useState(null);
-  const [notification, setNotification] = useState(null);
+	const [blogs, setBlogs] = useState([]);
+	const [user, setUser] = useState(null);
+	const [notification, setNotification] = useState(null);
+	const [showBlogForm, setShowBlogForm] = useState(false);
 
-  useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
-  }, []);
+	useEffect(() => {
+		blogService.getAll().then((blogs) => setBlogs(blogs));
+	}, []);
 
-  const handleLogout = () => {
-    // removing the token from the local storage
-    window.localStorage.removeItem("blogListUser");
+	const handleLogout = () => {
+		// removing the token from the local storage
+		window.localStorage.removeItem("blogListUser");
 
-    // changing the token from blogs service
-    blogService.setToken(null);
+		// changing the token from blogs service
+		blogService.setToken(null);
 
-    // changing the user
-    setUser(null);
-  };
+		// changing the user
+		setUser(null);
+	};
 
-  return (
-    <div>
-      <h1 style={{ color: "green" }}>Welcome to my Bloglist Application :-)</h1>
-      {notification && <NotificationMessage notification={notification} />}
+	return (
+		<div>
+			<h1 style={{ color: "green" }}>
+				Welcome to my Bloglist Application :-)
+			</h1>
+			{notification && (
+				<NotificationMessage notification={notification} />
+			)}
 
-      {user ? (
-        <>
-          <h2>blogs</h2>
-          <br />
-          <h3>
-            {user.name} has logged in{" "}
-            <button onClick={handleLogout}>logout</button>
-          </h3>
-          <CreateBlogForm setNotification={setNotification} />
-          <br />
-          <u>
-            {blogs.map((blog) => (
-              <li key={blog.id}>
-                {blog.title} {blog.author}
-              </li>
-            ))}
-          </u>
-        </>
-      ) : (
-        <>
-          <LoginForm setUser={setUser} setNotification={setNotification} />
-        </>
-      )}
-    </div>
-  );
+			{user ? (
+				<>
+					<h2>blogs</h2>
+					<br />
+					<h3>
+						{user.name} has logged in{" "}
+						<button onClick={handleLogout}>logout</button>
+					</h3>
+					{showBlogForm && (
+						<CreateBlogForm setNotification={setNotification} />
+					)}
+					<br />
+					<button onClick={() => setShowBlogForm(!showBlogForm)}>
+						{showBlogForm ? "cancel" : "create new blog"}
+					</button>
+
+					<br />
+					<u>
+						{blogs.map((blog) => (
+							<li key={blog.id}>
+								{blog.title} {blog.author}
+							</li>
+						))}
+					</u>
+				</>
+			) : (
+				<>
+					<LoginForm
+						setUser={setUser}
+						setNotification={setNotification}
+					/>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default App;
